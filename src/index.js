b13a26b3d172146140759a9a450dd4141201e88f
Madis Abel <mabel@twilio.com>
2018-12-19 16:33:20 +0200

    Use @twilio/flex-ui@1.3.0 (#3)

--- src/index.js
+++ src/index.js
@@ -6,21 +6,19 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-const configStorageKey = "FLEX_CONFIG";
 const mountNode = document.getElementById("root");
 
 window.onload = () => {
   const predefinedConfig = window.appConfig || {};
-  const runtimeConfig = getRuntimeConfig();
 
   const configuration = {
     ...predefinedConfig,
-    ...runtimeConfig
   };
 
   Flex
     .progress(mountNode)
-    .Manager.create(configuration)
+    .provideLoginInfo(configuration, mountNode)
+    .then(() => Flex.Manager.create(configuration))
     .then(manager => renderApp(manager))
     .catch(error => handleError(error));
 };
@@ -34,40 +32,6 @@ function renderApp(manager) {
 
 function handleError(error) {
   console.error("Failed to initialize Flex", error);
-
-  const missingAccountSid = error instanceof Flex.ConfigError && error.key === "accountSid";
-
-  if (!missingAccountSid) {
-    throw error;
-  }
-
-  ReactDOM.render(
-    <Flex.RuntimeLoginView
-      onSuccess={(loginData, runtimeDomain) => {
-        setRuntimeConfig(loginData, runtimeDomain);
-        window.location.reload();
-      }}
-    />,
-    mountNode
-  );
-}
-
-function setRuntimeConfig(loginData, runtimeDomain) {
-    const config = {
-        serviceBaseUrl: runtimeDomain,
-        sso: {
-            accountSid: loginData.accountSid
-        }
-    };
-    const serializedConfig = JSON.stringify(config);
-    localStorage.setItem(configStorageKey, serializedConfig);
-}
-
-function getRuntimeConfig() {
-    const serializedConfig = localStorage.getItem(configStorageKey);
-    localStorage.removeItem(configStorageKey);
-    const config = JSON.parse(serializedConfig || "{}");
-    return config;
 }
 
 registerServiceWorker();
