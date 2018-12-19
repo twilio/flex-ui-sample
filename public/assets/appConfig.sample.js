09ae1d585371193a570a33caee667ba56ec58d00
Madis Abel <mabel@twilio.com>
2018-12-19 13:42:56 +0200

    Use @twilio/flex-ui@1.3.0

--- public/assets/appConfig.sample.js
+++ public/assets/appConfig.sample.js
@@ -1,7 +1,3 @@
-var serviceBaseUrl = window.location.origin;
-// Set it to your Twilio Runtime domain:
-// serviceBaseUrl = "https://somedomain.twil.io";
-
 // set your account sid here
 var accountSid = "AC...";
 
@@ -9,7 +5,6 @@ var accountSid = "AC...";
 var appConfig = {
   // Uncomment the following lines to enable automatic log in
   //
-  // serviceBaseUrl: serviceBaseUrl  + "/",
   // sso: {
   //   accountSid: accountSid
   // }
